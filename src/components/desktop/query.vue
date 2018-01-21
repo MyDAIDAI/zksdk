475e62a0e65a05fbfceafbe77eed565253179667
daidai <571143755@qq.com>
2018-01-21 16:21:17 +0800

    电脑端用户查询

--- src/components/desktop/query.vue
+++ src/components/desktop/query.vue
@@ -1,39 +1,53 @@
 <template>
   <div class="query">
     <layout :activeIndex="activeIndex">
-      <el-row style="display: flex;justify-content: flex-end;margin-bottom: 1em">
+      <el-row style="display: flex;justify-content: flex-end;margin:30px;">
+        <el-col :span="10">
+           <el-date-picker
+            v-model="dateValue"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+            @change="queryDataFun">
+          </el-date-picker>
+        </el-col>
         <el-col :span="6">
-          <div style="margin-top: 15px;">
-            <el-input placeholder="请输入内容" v-model="queryData" >
-              <el-button slot="append" icon="el-icon-search"></el-button>
+          <div>
+            <el-input placeholder="请输入查询用户ID" v-model="queryData" @change="queryDataFun">
+              <el-button slot="append" icon="el-icon-search" @click="queryDataFun"></el-button>
             </el-input>
           </div>
         </el-col>
       </el-row>
-      <el-table
-        :data="tableData"
-        border
-        style="width: 100%">
-        <el-table-column
-          prop="name"
-          label="姓名">
-        </el-table-column>
-        <el-table-column
-          prop="status"
-          label="状态"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="time"
-          label="时间">
-        </el-table-column>
-      </el-table>
+      <div style="padding: 0 30px">
+        <el-table
+          :data="tableData"
+          border
+          style="width: 100%">
+          <el-table-column
+            prop="name"
+            label="姓名">
+          </el-table-column>
+          <el-table-column
+            prop="status"
+            label="状态"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="time"
+            label="时间">
+          </el-table-column>
+        </el-table>
+      </div>
     </layout>
   </div>
 </template>
 
 <script>
   import layout from '@/layouts/Desktop'
+  import {getData} from '@/util/http'
+  import {ERR_OK} from '@/api/config'
   export default {
     name: 'query',
     components: {
@@ -42,11 +56,37 @@
     data () {
       return {
         queryData: '',
-        tableData: []
+        tableData: [],
+        dateValue: ''
       }
     },
     created () {
       this.activeIndex = '3'
+    },
+    mounted () {
+      this.getDataList()
+    },
+    methods: {
+      getDataList () {
+        getData('/zk/listRecord').then((res) => {
+          if (res.code === ERR_OK) {
+            this.tableData = eval(res.data)
+          }
+        }) 
+      },
+      queryDataFun () {
+        let start = Date.parse(new Date(this.dateValue[0]))
+        let end = Date.parse(new Date(this.dateValue[1]))
+        getData('/zk/listRecord', {
+          userId: this.queryData,
+          start: start,
+          end: end
+        }).then((res) => {
+          if (res.code === ERR_OK) {
+            this.tableData = eval(res.data)
+          }
+        })
+      }
     }
   }
 </script>
