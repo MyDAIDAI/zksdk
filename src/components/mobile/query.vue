5f1051e253997e202fe9981e3d9b2d4c0d49d4b6
daidai <571143755@qq.com>
2018-01-24 15:08:01 +0800

    手机端开门以及查询界面

---
+++ src/components/mobile/query.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <layout :title="title" :isBack="back" :active="active">
+      <list-view @on-pullup="pullupHandler" @on-pulldown="pulldownHandler" :loading="loading" :end="end">
+        <template v-for="item in list">
+          <flexbox align="center" class="list-view-item">
+            <flexbox-item>
+              <div style="padding: 10px; padding-left: 15px">
+                <h4 style="margin: 0px; color: #464c5b">{{item.name}}</h4>
+                <div style="color: #657180; font-size: 12px">
+                  <span>状态：{{item.status}}</span> &nbsp; &nbsp; <span>时间：{{item.time}}</span>
+                </div>
+              </div>
+            </flexbox-item>
+          </flexbox>
+          <divider></divider>
+        </template>
+      </list-view>
+    </layout>
+  </div>
+</template>
+
+<script>
+  import Layout from '@/layouts/Mobile'
+  import ListView from '@/base/list-view'
+  import Divider from '@/base/divider'
+  import {Flexbox, FlexboxItem} from '@/base/flexbox'
+  // import Search from '@/base/search'
+  import {getData, deleteData, putData} from '@/util/http'
+  import {dateToTimestamp, timestampToDate} from '@/util/date'
+  import {ERR_OK} from '@/api/config'
+  const PAGE_SIZE = 15
+  export default {
+    name: 'list',
+    components: {
+      Layout,
+      ListView,
+      Flexbox,
+      FlexboxItem,
+      Divider,
+      // Search
+    },
+    created () {
+      this.title = '中控sdk'
+      this.back = false
+      this.active = 2
+    },
+    data () {
+      return {
+        list: [],
+        loading: true,
+        end: false,
+        total: 0,
+        pageSize: PAGE_SIZE,
+        lastPage: 0,
+        page: 1
+      }
+    },
+    mounted () {
+      this.getDataList()
+    },
+    methods: {
+      getDataList (type) {
+        if (type === 'query') {
+          this.page = 1
+        }
+        let start = ''
+        let end = ''
+        if (this.dateValue) {
+          start = this.dateValue[0] ? dateToTimestamp(this.dateValue[0]) : ''
+          end = this.dateValue[1] ? dateToTimestamp(this.dateValue[1]) : ''
+        }
+        getData('/zk/listRecord',{
+          name: this.queryData,
+          startTime: start,
+          endTime: end,
+          page: this.page,
+          pageSize: PAGE_SIZE
+        }).then((res) => {
+          if (res.code === ERR_OK) {
+            this.total = res.data.notes
+            let data = res.data.list ? eval(res.data.list) : []
+            this.list = data.map(ele => {
+              return {
+                name: ele.name,
+                status: ele.status,
+                time: timestampToDate(ele.time)
+              }
+            })
+          }
+        }) 
+      },
+      pullupHandler (e) {
+        this.page += 1
+        if (this.page > this.lastPage) {
+          this.loading = false
+          this.end = true
+          return
+        }
+        console.log(this.page)
+        this._getUserList()
+      },
+      pulldownHandler (e) {
+        this.page = 1
+        this._getUserList()
+      }
+    }
+  }
+</script>
--- src/router/mobile.js
+++ src/router/mobile.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import List from '@/components/mobile/list'
 import Create from '@/components/mobile/create'
+import Query from '@/components/mobile/query'
+import Open from '@/components/mobile/open'
 
 Vue.use(Router)
 
@@ -16,6 +18,16 @@ export default new Router({
       path: '/create',
       name: 'Create',
       component: Create
+    },
+    {
+      path: '/query',
+      name: 'Query',
+      component: Query
+    },
+    {
+      path: '/open',
+      name: 'Open',
+      component: Open
     },
   ]
 })
